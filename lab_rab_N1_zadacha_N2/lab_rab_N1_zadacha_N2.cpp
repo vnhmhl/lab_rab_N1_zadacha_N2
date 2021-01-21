66af026fd8a2103d6f13950d1a903a40d66f844a
IVANOV <79323@DESKTOP-7NSDFUG>
2021-01-21 11:35:40 +0500

    Добавьте файлы проекта.

---
+++ lab_rab_N1_zadacha_N2/lab_rab_N1_zadacha_N2.cpp
@@ -0,0 +1,36 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	
+	cout << "Print number m:";
+	int m, a;
+	cin >> a;
+	cout << "Print number n:";
+	int n, b;
+	cin >> b;
+	n = b;
+	m = a;
+	cout << "Rachet:" << endl;
+	cout << "Result 1: " << n-- - m << endl;
+	n = b;
+	m = a;
+	cout << "Result 2: " << ((m-- < n) ? "true" : "false") << endl;
+	n = b;
+	m = a;
+	cout << "Result 3: " << ((n++ > m) ? "true" : "false") << endl;
+	cout << "Rachet:" << endl;
+	n = b;
+	m = a;
+	cout << "Result 1: " << n-- - m << endl;
+	cout << "Result 2: " << ((m-- < n) ? "true" : "false") << endl;
+	cout << "Result 3: " << ((n++ > m) ? "true" : "false") << endl;
+	system("pause");
+	return 0;
+}
+
+
